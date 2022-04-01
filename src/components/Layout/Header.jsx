0d9fce018888002847824d083c3df7b18143391a
chaiqou <lomtadzenikusha@gmail.com>
2022-04-01 16:36:41 +0400

    new favicon , change site name

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -8,7 +8,7 @@ const Header = ({ showCartHandler }) => {
         className="flex text-white justify-between items-center fixed top-0 left-0 w-full h-[5rem] 
       bg-yellow-900 py-0 px-[10%] z-10 shadow-normal "
       >
-        <div>React Meals</div>
+        <div>Talga Meals</div>
         <HeaderButton showCartHandler={showCartHandler} />
       </header>
       <div className="w-full z-0 overflow-hidden h-[22rem]">
