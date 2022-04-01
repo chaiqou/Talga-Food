94ec13b8729e79e19027203a85c84076eff80715
chaiqou <lomtadzenikusha@gmail.com>
2022-04-01 17:17:51 +0400

    Animated header button if context items length < 0

--- src/components/Layout/HeaderButton.jsx
+++ src/components/Layout/HeaderButton.jsx
@@ -1,20 +1,30 @@
 import { AiOutlineShoppingCart } from "react-icons/ai";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import CartContext from "../../context/CartContext";
 
 const HeaderButton = ({ showCartHandler }) => {
+  const [btnIsHighlited, setBtnIsHighlited] = useState(false);
   const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
 
-  const itemsInCart = cartCtx.items.reduce((currentItem, item) => {
+  const itemsInCart = items.reduce((currentItem, item) => {
     return currentItem + item.amount;
   }, 0);
 
+  const headerButtonStyles = `cursor-pointer border-0 rounded-3xl bg-[#4d1601] text-white px-[3rem] py-[0.75rem] 
+  flex justify-around items-center font-bold hover:bg-[#2c0d00] active:bg-[#2c0d00] ${
+    btnIsHighlited ? "animate-bounce-in" : ""
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlited(true);
+  }, [items]);
+
   return (
-    <button
-      onClick={showCartHandler}
-      className="cursor-pointer border-0 rounded-3xl bg-[#4d1601] text-white px-[3rem] py-[0.75rem] 
-flex justify-around items-center font-bold hover:bg-[#2c0d00] active:bg-[#2c0d00]"
-    >
+    <button onClick={showCartHandler} className={headerButtonStyles}>
       <span className="w-[1.35rem] h-[1.35rem] mr-3">
         <AiOutlineShoppingCart />
       </span>
